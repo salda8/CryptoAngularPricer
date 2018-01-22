d701e44b823db0e13a1592310ea4c13353ea3982
salda8 <saldicek@gmail.com>
2018-01-22 13:11:54 +0100

    Signed commit (I hope)

--- src/app/mocks/mock.ts
+++ src/app/mocks/mock.ts
@@ -1,2 +1,15 @@
-export class Mock {
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class PriceDetailsData implements InMemoryDbService {
+    createDb() {
+        let books = [
+            { id: 1, name: 'Core Java' },
+            { id: 2, name: 'Angular 2' },
+            { id: 3, name: 'Hibernate' }
+        ];
+        return { books };
+    }
+
+
+
 }
